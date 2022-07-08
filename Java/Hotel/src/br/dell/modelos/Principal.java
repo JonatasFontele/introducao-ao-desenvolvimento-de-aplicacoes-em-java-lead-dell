a28cebcddc9c39669ab850199b4cdac1bf784e5b
Jonatas Dourado <jonatasfontele@gmail.com>
2022-07-08 15:30:13 -0300

    Exceptions treatment included

--- Java/Hotel/src/br/dell/modelos/Principal.java
+++ Java/Hotel/src/br/dell/modelos/Principal.java
@@ -1,45 +1,69 @@
 package br.dell.modelos;
 
 import javax.swing.*;
+import java.util.InputMismatchException;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		JTextField cpfField = new JTextField();
-		JTextField rgField = new JTextField();
-		JTextField nomeField = new JTextField();
-		JTextField idadeField = new JTextField();
-		JTextField enderecoField = new JTextField();
-		JTextField numeroField = new JTextField();
+    public static void main(String[] args) {
+        JTextField cpfField = new JTextField();
+        JTextField rgField = new JTextField();
+        JTextField nomeField = new JTextField();
+        JTextField idadeField = new JTextField();
+        JTextField enderecoField = new JTextField();
+        JTextField numeroField = new JTextField();
 
-		Object campos[] = { "CPF:", cpfField, "RG:", rgField, "Nome:", nomeField, "Idade:", idadeField, "Endereço:",
-				enderecoField, "Número do quarto:", numeroField };
+        Object campos[] = { "CPF:", cpfField, "RG:", rgField, "Nome:", nomeField, "Idade:", idadeField, "EndereÃ§o:",
+                enderecoField, "NÃºmero do quarto:", numeroField };
 
-		ImageIcon iconAccept = new ImageIcon("src/imagens/accept.png");
-		ImageIcon iconCancel = new ImageIcon("src/imagens/cancel.png");
+        while(true){
+            int option = JOptionPane.showConfirmDialog(null, campos, "Cadastrar hÃ³spede", JOptionPane.OK_CANCEL_OPTION);
+            if (option == JOptionPane.OK_OPTION) {
+                /*
+                 * for (Object campo : campos) { if (campo instanceof JTextField) {
+                 * System.out.println(((JTextField) campo).getText()); } }
+                 */
 
-		int option = JOptionPane.showConfirmDialog(null, campos, "Cadastrar hóspede", JOptionPane.OK_CANCEL_OPTION);
-		if (option == JOptionPane.OK_OPTION) {
-			/*
-			 * for (Object campo : campos) { if (campo instanceof JTextField) {
-			 * System.out.println(((JTextField) campo).getText()); } }
-			 */
-			Hospede hospede = new Hospede();
+                try {
+                    Hospede hospede = new Hospede();
+                    hospede.setCPF(Long.parseLong(((JTextField) campos[1]).getText()));
+                    hospede.setRG(Long.parseLong(((JTextField) campos[3]).getText()));
+                    if(validaNome(((JTextField) campos[5]).getText()))
+                        hospede.setNome(((JTextField) campos[5]).getText());
+                    else {
+                        System.err.println("Does not match ^[a-zA-Z\\s]+ regex");
+                        JOptionPane.showMessageDialog(null, "Digite um nome vÃ¡lido");
+                        continue;
+                    }
+                    hospede.setIdade(Integer.parseInt(((JTextField) campos[7]).getText()));
+                    hospede.setEndereco(((JTextField) campos[9]).getText());
 
-			hospede.setCPF(Long.parseLong(((JTextField) campos[1]).getText()));
-			hospede.setRG(Long.parseLong(((JTextField) campos[3]).getText()));
-			hospede.setNome(((JTextField) campos[5]).getText());
-			hospede.setIdade(Integer.parseInt(((JTextField) campos[7]).getText()));
-			hospede.setEndereco(((JTextField) campos[9]).getText());
+                    Quarto quarto = new Quarto(Integer.parseInt(((JTextField) campos[11]).getText()), hospede);
 
-			Quarto quarto = new Quarto(Integer.parseInt(((JTextField) campos[11]).getText()), hospede);
+                    Object retorno[] = { "CPF:", hospede.getCPF(), "RG:", hospede.getRG(), "Nome:", hospede.getNome(), "Idade:",
+                            hospede.getIdade(), "EndereÃ§o:", hospede.getEndereco(), "NÃºmero do quarto:", quarto.getNumero() };
 
-			Object retorno[] = { "CPF:", hospede.getCPF(), "RG:", hospede.getRG(), "Nome:", hospede.getNome(), "Idade:",
-					hospede.getIdade(), "Endereço:", hospede.getEndereco(), "Número do quarto:", quarto.getNumero() };
+                    JOptionPane.showMessageDialog(null, retorno, "HÃ³spede cadastrado com sucesso", JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                }catch (NumberFormatException | InputMismatchException exception) {
+                    System.err.println(exception);
+                    JOptionPane.showMessageDialog(null, "Digite um valor vÃ¡lido");
+                }catch (Exception exception) {
+                    System.err.println(exception);
+                    JOptionPane.showMessageDialog(null, "Houve um problema com o valor digitado");
+                }
 
-			JOptionPane.showMessageDialog(null, retorno, "Hóspede cadastrado com sucesso", JOptionPane.PLAIN_MESSAGE,
-					iconAccept);
-		} else
-			JOptionPane.showMessageDialog(null, null, "Cadastramento cancelado", JOptionPane.PLAIN_MESSAGE, iconCancel);
-	}
+            } else {
+                //JOptionPane.showMessageDialog(null, "Nada foi cadastrado", "Cadastramento cancelado", JOptionPane.PLAIN_MESSAGE, iconCancel);
+                int option2 =JOptionPane.showConfirmDialog(null, "Deseja realmente fechar?", "Nada foi cadastrado", JOptionPane.YES_NO_OPTION);
+                if (option2 == JOptionPane.YES_OPTION)
+                    break;
+            }
+
+        }
+    }
+    public static boolean validaNome(String nome) {
+        String expression = "^[a-zA-Z\\s]+";
+        return nome.matches(expression);
+    }
 }
